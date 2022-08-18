6e9619af3bb113d428a5cefb62ad0f1f05ade31c
Vincenzo Marcovecchio <55591572+VincenzoMarcovecchio@users.noreply.github.com>
2022-08-18 18:15:44 +0200

    test

---
+++ dist/js/chunk-2d0c7e68.043c70d3.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c7e68"],{"531e":function(n,e,u){"use strict";u.r(e);var t=function(){var n=this,e=n._self._c;return e("router-view")},c=[],r=u("2877"),l={},o=Object(r["a"])(l,t,c,!1,null,null,null);e["default"]=o.exports}}]);
